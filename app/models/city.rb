32fa5644fa5c76c6ba896d38316623581828b4ce
megan <megan.schertz@gmail.com>
2021-03-23 16:02:59 -0500

    commit tree 6849dfcb08d5a02c63e1716e4a7846d464b115b2

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,27 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(start_date, end_date)
+    openings = []
+    self.listings.each do |listing|
+     if listing.is_available?(start_date, end_date)
+        openings << listing
+      end
+    end
+    openings
+  end
+
+  def self.highest_ratio_res_to_listings
+    
+  end
+
+  def self.most_res
+
+  end
+
+  def total_reservations
+    self.listings.sum{|listing| listing.reservations.count}
+  end
+
 end
 
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,28 @@ class Listing < ActiveRecord::Base
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
   
+  validates :address, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+
+  
+  def is_available?(start_date, end_date)
+    day_one = start_date.to_date
+    day_two = end_date.to_date
+    reservations.each do |reservation|
+      if day_one < reservation.checkin && day_two > reservation.checkin
+        return false
+      elsif day_one < reservation.checkout && day_two > reservation.checkout
+        return false
+      elsif reservation.checkin < day_one && reservation.checkout > day_one
+        return false
+      elsif reservation.checkin < day_two && reservation.checkout > day_two
+        return false
+      end
+    end
+    return true
+  end
+
 end
